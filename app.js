b3498b8228b0ab7d3b3c242d8d7850b0f9595ac7
Nate Hayflick <nhayflick@gmail.com>
2015-02-21 17:20:39 -0800

    Style and add browsing from lightbox

--- app.js
+++ app.js
@@ -2,49 +2,108 @@
 var PHOTOSET_ID = '72157626579923453';
 var API_KEY_FLICKR = '21e829380250527a8e863d4d705f3413';
 
-var AppController = (function () {
+function App () {
 
-  var photoSetData = [];
+  this.photoSetData = {};
+  this.photoCollection = [];
+  this.currentLightboxIndex = 0;
 
   // Renders a Flickr PhotoSet Data Object to the DOM
-  function render (data) {
+  this.init = function (data) {
+    var that = this;
+    var thumbnailContainer = document.getElementById('thumbnail-container');
     photoSetData = data;
-    var photoCollection = photoSetData.photo;
-    for (var i = 0; i < photoCollection.length; i++) {
-      var el = document.createElement('IMG');
-      el.src = constructPhotoUrl(photoCollection[i], true);
-      el.className = 'my-class';
-      el.dataset.id = photoCollection[i].id;
-      el.dataset.index = i;
-      // el.height = '100';
-      // el.width = '100';
-      document.getElementById('results-div').appendChild(el);
+    this.photoCollection = photoSetData.photo;
+    document.getElementById('set-label').innerHTML = photoSetData.title;
+    for (var i = 0; i < this.photoCollection.length; i++) {
+      var el = renderThumbnail(photo);
+      thumbnailContainer.appendChild(el);
       // Set up click listener to launch lightbox
       el.addEventListener('click', function (e) {
-        launchLightBox(e.target.dataset.index)
+        that.launchLightBox(parseInt(e.target.dataset.index));
       }, false);
     }
+    // initialize lightbox listeners
+    var nextImageButton = document.getElementById('next-image');
+    var previousImageButton = document.getElementById('previous-image');
+    document.getElementById('lightbox').addEventListener('click', that.hideLightBox, false);
+    nextImageButton.addEventListener('click', that.nextImage.bind(that), true);
+    nextImageButton.addEventListener('mouseover', that.prefetchNextImage.bind(that), true);
+    previousImageButton.addEventListener('click', that.previousImage.bind(that), true);
+    previousImageButton.addEventListener('mouseover', that.prefetchPreviousImage.bind(that), true);
+  }
+
+  this.renderThumbnail = function (photo) {
+    var el = document.createElement('IMG');
+    el.src = this.constructPhotoUrl(this.photoCollection[i], true);
+    el.className = 'img-thumb';
+    el.dataset.id = this.photoCollection[i].id;
+    el.dataset.index = i;
+    return el;
   }
 
   // Launchs a fullscreen lightbox displaying an image in the photoset
-  function launchLightBox(index) {
+  this.launchLightBox = function (index) {
+    var that = this;
+    this.currentLightboxIndex = index;
     document.getElementById('lightbox').className = 'visible';
-    document.getElementById('lightbox').addEventListener('click', hideLightBox, false);
-    document.getElementById('lightbox-image').src = constructPhotoUrl(photoSetData.photo[index], false);
-    document.getElementById('lightbox-label').innerHTML = photoSetData.photo[index].title;
+    this.renderLightBoxImage();
     // Prevent background scrolling
     document.body.className = 'lightbox-open';
   }
 
+  // Calls render on the next image in the collection, looping back if necessary
+  this.nextImage = function (e) {
+    e.stopPropagation();
+    this.currentLightboxIndex = (this.currentLightboxIndex + 1) % this.photoCollection.length;
+    this.renderLightBoxImage();
+    this.prefetchNextImage();
+  }
+
+  // Calls render on the previous image in the collection, looping back if necessary
+  this.previousImage = function (e) {
+    e.stopPropagation();
+    this.currentLightboxIndex = this.currentLightboxIndex > 0 ? this.currentLightboxIndex - 1 : this.photoCollection.length - 1;
+    this.renderLightBoxImage();
+    this.prefetchPreviousImage();
+  }
+
+// Prefetch previous image and next image to improve app responsiveness
+  this.prefetchNextImage = function () {
+    var nextLightboxIndex = (this.currentLightboxIndex + 1) % this.photoCollection.length;
+    var nextLightboxImage = document.createElement('IMG').src = this.constructPhotoUrl(photoSetData.photo[nextLightboxIndex], false);
+  }
+  this.prefetchPreviousImage = function () {
+    var previousLightboxIndex = this.currentLightboxIndex > 0 ? this.currentLightboxIndex - 1 : this.photoCollection.length - 1;
+    var previousLightboxImage = document.createElement('IMG').src = this.constructPhotoUrl(photoSetData.photo[previousLightboxIndex], false);
+  }
+
+  // Renders a selected image
+  this.renderLightBoxImage = function () {
+    var lightboxImage = document.getElementById('lightbox-image');
+    var lightboxLabel = document.getElementById('lightbox-label');
+    lightboxImage.className = 'lightbox-image loading';
+    lightboxImage.addEventListener("load", (function () {
+      // Reenable navigation UI
+      lightboxImage.className = 'lightbox-image';
+    }).bind(this));
+    lightboxLabel.innerHTML = photoSetData.photo[this.currentLightboxIndex].title;
+    lightboxImage.src = this.constructPhotoUrl(photoSetData.photo[this.currentLightboxIndex], false);
+  }
+
   // Hide the lightbox
-  function hideLightBox() {
-    document.getElementById('lightbox').className = '';
+  this.hideLightBox = function (e) {
+    var lightboxEl = document.getElementById('lightbox');
+    // Don't bubble event from other elements
+    if (e.target.id != 'lightbox') return false;
+    lightboxEl.className = '';
+    document.getElementById('lightbox-image').class = 'lightbox-image';
     document.body.className = '';
-    // TODO: Destroy Listener
+    this.currentLightboxIndex = 0;
   }
 
   // Construct a src url for an image as a thumbnail or full image
-  function constructPhotoUrl(photoData, thumbnail) {
+  this.constructPhotoUrl = function (photoData, thumbnail) {
     var imageExt = thumbnail ? '_t.jpg' : '.jpg';
     return 'https://farm'
       + photoData.farm 
@@ -56,11 +115,11 @@ var AppController = (function () {
       + photoData.secret
       + imageExt;
   }
+}
 
-    // Wait for DOM to initialize - works on all modern browsers after IE8
-  document.addEventListener("DOMContentLoaded", function(event) { 
-    var photoSet = new PhotoSet(PHOTOSET_ID);
-    photoSet.fetch(render);
-  });
-
-})();
+ // Wait for DOM to initialize - works on all modern browsers after IE8
+document.addEventListener("DOMContentLoaded", function(event) { 
+  var app = new App;
+  var photoSet = new PhotoSet(PHOTOSET_ID);
+  photoSet.fetch(app.init.bind(app));
+});
