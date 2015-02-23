6621823e575bdf1a03b2c1c147264feb0e04eebd
Nate Hayflick <nhayflick@gmail.com>
2015-02-22 23:25:09 -0800

    Clean up, add keyboard listeners, refactor to module pattern

--- app.js
+++ app.js
@@ -1,125 +1,150 @@
-// Define some constants
-var PHOTOSET_ID = '72157626579923453';
-var API_KEY_FLICKR = '21e829380250527a8e863d4d705f3413';
+var PhotosetApp = (function() {
 
-function App () {
+    // Private API...
 
-  this.photoSetData = {};
-  this.photoCollection = [];
-  this.currentLightboxIndex = 0;
+    // Define some constants
+    var PHOTOSET_ID = '72157626579923453';
+    var API_KEY_FLICKR = '21e829380250527a8e863d4d705f3413';
 
-  // Renders a Flickr PhotoSet Data Object to the DOM
-  this.init = function (data) {
-    var that = this;
-    var thumbnailContainer = document.getElementById('thumbnail-container');
-    photoSetData = data;
-    this.photoCollection = photoSetData.photo;
-    document.getElementById('set-label').innerHTML = photoSetData.title;
-    for (var i = 0; i < this.photoCollection.length; i++) {
-      var el = renderThumbnail(photo);
-      thumbnailContainer.appendChild(el);
-      // Set up click listener to launch lightbox
-      el.addEventListener('click', function (e) {
-        that.launchLightBox(parseInt(e.target.dataset.index));
-      }, false);
-    }
-    // initialize lightbox listeners
-    var nextImageButton = document.getElementById('next-image');
-    var previousImageButton = document.getElementById('previous-image');
-    document.getElementById('lightbox').addEventListener('click', that.hideLightBox, false);
-    nextImageButton.addEventListener('click', that.nextImage.bind(that), true);
-    nextImageButton.addEventListener('mouseover', that.prefetchNextImage.bind(that), true);
-    previousImageButton.addEventListener('click', that.previousImage.bind(that), true);
-    previousImageButton.addEventListener('mouseover', that.prefetchPreviousImage.bind(that), true);
-  }
+    var photoCollection = [];
+    var currentLightboxIndex = -1;
 
-  this.renderThumbnail = function (photo) {
-    var el = document.createElement('IMG');
-    el.src = this.constructPhotoUrl(this.photoCollection[i], true);
-    el.className = 'img-thumb';
-    el.dataset.id = this.photoCollection[i].id;
-    el.dataset.index = i;
-    return el;
-  }
-
-  // Launchs a fullscreen lightbox displaying an image in the photoset
-  this.launchLightBox = function (index) {
-    var that = this;
-    this.currentLightboxIndex = index;
-    document.getElementById('lightbox').className = 'visible';
-    this.renderLightBoxImage();
-    // Prevent background scrolling
-    document.body.className = 'lightbox-open';
-  }
-
-  // Calls render on the next image in the collection, looping back if necessary
-  this.nextImage = function (e) {
-    e.stopPropagation();
-    this.currentLightboxIndex = (this.currentLightboxIndex + 1) % this.photoCollection.length;
-    this.renderLightBoxImage();
-    this.prefetchNextImage();
-  }
-
-  // Calls render on the previous image in the collection, looping back if necessary
-  this.previousImage = function (e) {
-    e.stopPropagation();
-    this.currentLightboxIndex = this.currentLightboxIndex > 0 ? this.currentLightboxIndex - 1 : this.photoCollection.length - 1;
-    this.renderLightBoxImage();
-    this.prefetchPreviousImage();
-  }
-
-// Prefetch previous image and next image to improve app responsiveness
-  this.prefetchNextImage = function () {
-    var nextLightboxIndex = (this.currentLightboxIndex + 1) % this.photoCollection.length;
-    var nextLightboxImage = document.createElement('IMG').src = this.constructPhotoUrl(photoSetData.photo[nextLightboxIndex], false);
-  }
-  this.prefetchPreviousImage = function () {
-    var previousLightboxIndex = this.currentLightboxIndex > 0 ? this.currentLightboxIndex - 1 : this.photoCollection.length - 1;
-    var previousLightboxImage = document.createElement('IMG').src = this.constructPhotoUrl(photoSetData.photo[previousLightboxIndex], false);
-  }
-
-  // Renders a selected image
-  this.renderLightBoxImage = function () {
+    // Grab DOM elements for manipulation
+    var photoSetLabel = document.getElementById('set-label');
     var lightboxImage = document.getElementById('lightbox-image');
     var lightboxLabel = document.getElementById('lightbox-label');
-    lightboxImage.className = 'lightbox-image loading';
-    lightboxImage.addEventListener("load", (function () {
-      // Reenable navigation UI
-      lightboxImage.className = 'lightbox-image';
-    }).bind(this));
-    lightboxLabel.innerHTML = photoSetData.photo[this.currentLightboxIndex].title;
-    lightboxImage.src = this.constructPhotoUrl(photoSetData.photo[this.currentLightboxIndex], false);
-  }
+    var thumbnailContainer = document.getElementById('thumbnail-container');
+    var nextImageButton = document.getElementById('next-image');
+    var previousImageButton = document.getElementById('previous-image');
+    var lightboxContainer = document.getElementById('lightbox');
 
-  // Hide the lightbox
-  this.hideLightBox = function (e) {
-    var lightboxEl = document.getElementById('lightbox');
-    // Don't bubble event from other elements
-    if (e.target.id != 'lightbox') return false;
-    lightboxEl.className = '';
-    document.getElementById('lightbox-image').class = 'lightbox-image';
-    document.body.className = '';
-    this.currentLightboxIndex = 0;
-  }
+    // initialize DOM listeners
+    lightboxContainer.addEventListener('click', hideLightBox, false);
+    nextImageButton.addEventListener('click', nextImage, true);
+    previousImageButton.addEventListener('click', previousImage, true);
+    window.addEventListener('keydown', handleKeyDown, false);
 
-  // Construct a src url for an image as a thumbnail or full image
-  this.constructPhotoUrl = function (photoData, thumbnail) {
-    var imageExt = thumbnail ? '_t.jpg' : '.jpg';
-    return 'https://farm'
-      + photoData.farm 
-      + '.staticflickr.com/'
-      + photoData.server
-      + '/'
-      + photoData.id
-      + '_'
-      + photoData.secret
-      + imageExt;
-  }
-}
+    // Renders a Flickr photoset to the DOM
+    function render(data) {
+        photoCollection = data.photo;
 
- // Wait for DOM to initialize - works on all modern browsers after IE8
-document.addEventListener("DOMContentLoaded", function(event) { 
-  var app = new App;
-  var photoSet = new PhotoSet(PHOTOSET_ID);
-  photoSet.fetch(app.init.bind(app));
+        // Render photo collection title
+        photoSetLabel.innerHTML = data.title;
+
+        // Render thumbnail divs
+        for (var i = 0; i < photoCollection.length; i++) {
+
+            // Construct new div element
+            var el = document.createElement('DIV');
+            el.style.backgroundImage = 'url(' + constructPhotoUrl(photoCollection[i], true) + ')';
+            el.className = 'img-thumb';
+
+            // Set up click listener to launch lightbox
+            (function(i) {
+                el.addEventListener('click', function() {
+                    launchLightBox(i);
+                }, false);
+            })(i);
+            thumbnailContainer.appendChild(el);
+        }
+    }
+
+    // Construct a src url for an image as a thumbnail or full image
+    function constructPhotoUrl(photoData, thumbnail) {
+        var imageExt = thumbnail ? '_t.jpg' : '.jpg';
+        return 'https://farm' + photoData.farm + '.staticflickr.com/' + photoData.server + '/' + photoData.id + '_' + photoData.secret + imageExt;
+    }
+
+    // Launchs a fullscreen lightbox displaying an image in the photoset
+    function launchLightBox(index) {
+        currentLightboxIndex = index;
+        lightboxContainer.className = 'visible';
+
+        // Prefetch neighboring images once loaded
+        lightboxImage.addEventListener('load', (function() {
+            prefetchNextImage();
+            prefetchPreviousImage();
+            // Only call once
+            lightboxImage.removeEventListener('load', arguments.callee);
+        }));
+
+        // Render the currently selected image
+        renderLightBoxImage();
+        // Prevent background scrolling
+        document.body.className = 'lightbox-open';
+    }
+
+    // Renders a selected image
+    function renderLightBoxImage() {
+        lightboxLabel.innerHTML = photoCollection[currentLightboxIndex].title;
+        lightboxImage.src = constructPhotoUrl(photoCollection[currentLightboxIndex], false);
+    }
+
+    // Calls render on the next image in the collection, looping back if necessary
+    function nextImage(e) {
+        e.stopPropagation();
+        currentLightboxIndex = (currentLightboxIndex + 1) % photoCollection.length;
+        renderLightBoxImage();
+        prefetchNextImage();
+    }
+
+    // Calls render on the previous image in the collection, looping back if necessary
+    function previousImage(e) {
+        e.stopPropagation();
+        currentLightboxIndex = currentLightboxIndex > 0 ? currentLightboxIndex - 1 : photoCollection.length - 1;
+        renderLightBoxImage();
+        prefetchPreviousImage();
+    }
+
+    // Prefetch previous image and next image to improve app responsiveness
+    function prefetchNextImage() {
+        var nextLightboxIndex = (currentLightboxIndex + 1) % photoCollection.length;
+        var nextLightboxImage = document.createElement('IMG').src = constructPhotoUrl(photoCollection[nextLightboxIndex], false);
+    }
+
+    function prefetchPreviousImage() {
+        var previousLightboxIndex = currentLightboxIndex > 0 ? currentLightboxIndex - 1 : photoCollection.length - 1;
+        var previousLightboxImage = document.createElement('IMG').src = constructPhotoUrl(photoCollection[previousLightboxIndex], false);
+    }
+
+    // Let user swap lightbox images with arrow keys
+    function handleKeyDown(e) {
+        // Break out if lightbox isn't open
+        if (currentLightboxIndex == -1) return false;
+        // Handle Left Arrow
+        if (e.which == 37) {
+            previousImage(e);
+            // Handle Right Arrow
+        } else if (e.which == 39) {
+            nextImage(e);
+        }
+    }
+
+    // Hide the lightbox
+    function hideLightBox(e) {
+        // Don't bubble event from other elements
+        if (e.target.id != 'lightbox') return false;
+        // Toggle display none
+        lightboxContainer.className = '';
+        // Reenable scrolling
+        document.body.className = '';
+        // Reset lightbox index
+        currentLightboxIndex = -1;
+    }
+
+    // Public API...
+
+    return {
+        // Creates a new PhotoSet model and loads the set via AJAX 
+        init: function() {
+            var photoSet = new PhotoSet(PHOTOSET_ID, API_KEY_FLICKR);
+            photoSet.fetch(render);
+        }
+    }
+})();
+
+// Wait for DOM to initialize - works on all modern browsers after IE8
+document.addEventListener("DOMContentLoaded", function(event) {
+    // Kicks off the app
+    PhotosetApp.init();
 });
--- photoset.js
+++ photoset.js
@@ -1,24 +1,24 @@
-// Model for communicating with a Flickr Photoset API Endpoint
+// Model class for communicating with a Flickr Photoset API Endpoint
+function PhotoSet(id, apiKey) {
+    // Flickr photosetId
+    this.id = id;
+    // Public API Key
+    this.apiKey = apiKey;
+    // FLICKR API endpoint
+    this.urlBase = 'https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&photoset_id=';
 
-
-function PhotoSet (id) {
-  this.id = id;
-  this.urlBase = 'https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&photoset_id=';
-
-  this.constructFlickrURL = function () {
-    return 'https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key='
-    + API_KEY_FLICKR
-    + '&photoset_id='
-    + this.id
-    + '&format=json&nojsoncallback=1';
-  }
-
-  this.fetch = function (callback) {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onload = function () {
-      callback(JSON.parse(xmlhttp.responseText).photoset);
+    // Fetch the photoset and pass the results to a callback
+    this.fetch = function(callback) {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onload = function() {
+            callback(JSON.parse(xmlhttp.responseText).photoset);
+        }
+        xmlhttp.open('GET', this.constructFlickrURL(), true);
+        xmlhttp.send();
     }
-    xmlhttp.open('GET',this.constructFlickrURL(),true);
-    xmlhttp.send();
-  }
+
+    // Utility method for constructing the full API url
+    this.constructFlickrURL = function() {
+        return 'https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=' + this.apiKey + '&photoset_id=' + this.id + '&format=json&nojsoncallback=1';
+    }
 }
